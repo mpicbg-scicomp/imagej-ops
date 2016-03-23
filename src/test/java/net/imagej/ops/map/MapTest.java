8e76c89abebf433b3a6cf2c1a0f7f66f65cbec9f
Stefan Helfrich <stefan.helfrich@googlemail.com>
2016-03-23 19:20:55 +0100

    Add test for IItoIIParallel implementation (dirty)

--- src/test/java/net/imagej/ops/map/MapTest.java
+++ src/test/java/net/imagej/ops/map/MapTest.java
@@ -32,19 +32,39 @@ package net.imagej.ops.map;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
+import net.imagej.Dataset;
+import net.imagej.ImgPlus;
 import net.imagej.ops.AbstractOpTest;
 import net.imagej.ops.Op;
+import net.imagej.ops.OpService;
 import net.imagej.ops.Ops;
+import net.imagej.ops.features.AbstractFeatureTest;
+import net.imagej.ops.map.MapUnaryComputers.IIToII;
+import net.imagej.ops.map.MapUnaryComputers.IIToIIParallel;
+import net.imagej.ops.slice.SlicesII;
 import net.imagej.ops.special.computer.Computers;
+import net.imagej.ops.special.computer.UnaryComputerOp;
 import net.imagej.ops.special.inplace.BinaryInplaceOp;
 import net.imagej.ops.special.inplace.Inplaces;
 import net.imglib2.Cursor;
+import net.imglib2.IterableInterval;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.exception.IncompatibleTypeException;
 import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.integer.ByteType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+
+import ij.io.Opener;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.scijava.io.IOService;
+import org.scijava.plugin.Parameter;
 
 /**
  * Tests for {@link MapOp}s that are not covered in the auto generated tests.
@@ -168,9 +188,56 @@ public class MapTest extends AbstractOpTest {
 
 		assertImgSubOneEquals(in, out);
 	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Test
+	public void testIIToIIParallel() {
+		final String imageName = "confocal-series.tif";
+		final Img<UnsignedByteType> in = ImageJFunctions.wrapByte(new Opener().openImage(
+			MapTest.class.getResource(imageName).getPath()));
+		
+		Img<BitType> outLinear = null;
+		Img<BitType> outParallel = null;
+		try {
+			outLinear = in.factory().imgFactory(new BitType()).create(in, new BitType());
+			outParallel = in.factory().imgFactory(new BitType()).create(in, new BitType());
+		}
+		catch (IncompatibleTypeException exc) {
+			// TODO Auto-generated catch block
+			exc.printStackTrace();
+		}
+		UnaryComputerOp<RandomAccessibleInterval<UnsignedByteType>, RandomAccessibleInterval<BitType>> op = (UnaryComputerOp) ops.op(Ops.Threshold.Otsu.class, in);
+		
+		UnaryComputerOp<SlicesII<UnsignedByteType>, SlicesII<BitType>> mapperLinear =
+			(UnaryComputerOp) Computers.unary(ops, IIToII.class, SlicesII.class,
+				SlicesII.class, op);
+		
+		UnaryComputerOp<SlicesII<UnsignedByteType>, SlicesII<BitType>> mapperParallel =
+				(UnaryComputerOp) Computers.unary(ops, IIToIIParallel.class, SlicesII.class,
+					SlicesII.class, op);
+		
+		mapperLinear.compute1(new SlicesII<>(in, new int[]{0, 1}, true),
+			new SlicesII<>(outLinear, new int[]{0, 1}, true));
+		
+		mapperParallel.compute1(new SlicesII<>(in, new int[]{0, 1}, true),
+			new SlicesII<>(outParallel, new int[]{0, 1}, true));
+
+		assertImgEquals(outLinear, outParallel);
+	}
 
 	// -- helper methods --
 
+	private static <T> void assertImgEquals(Img<T> in,
+		Img<T> out)
+	{
+		final Cursor<T> inCursor = in.cursor();
+		final Cursor<T> outCursor = out.cursor();
+
+		while (inCursor.hasNext()) {
+			assertEquals(inCursor.next(), outCursor.next());
+		}
+	}
+	
 	private static void assertImgSubEquals(Img<ByteType> in1, Img<ByteType> in2,
 		Img<ByteType> out)
 	{

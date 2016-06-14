6d193bf43f408b15680156e15de7a6741312c128
tibuch <tim-oliver.buchholz@uni-konstanz.de>
2016-06-14 13:00:27 +0200

    Add IterableComputerSetProcessorTest.

---
+++ src/test/java/net/imagej/ops/features/sets/processors/IterableComputerSetProcessorTest.java
@@ -0,0 +1,107 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2016 Board of Regents of the University of
+ * Wisconsin-Madison, University of Konstanz and Brian Northan.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imagej.ops.features.sets.processors;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import net.imagej.ops.Ops.Stats.Max;
+import net.imagej.ops.Ops.Stats.Mean;
+import net.imagej.ops.features.sets.ComputerSet;
+import net.imagej.ops.features.sets.FirstOrderStatsComputerSet;
+import net.imagej.ops.features.sets.tables.DefaultComputerSetTableService;
+import net.imagej.table.Column;
+import net.imagej.table.Table;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.type.numeric.real.FloatType;
+
+import org.junit.Test;
+
+/**
+ * Test for the {@link IterableComputerSetProcessor}.
+ * 
+ * @author Tim-Oliver Buchholz, University of Konstanz
+ *
+ */
+public class IterableComputerSetProcessorTest extends AbstractComputerSetProcessorTest {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test
+	public void IterableComputerSetProcessorAllComputersAreActiveTest() {
+		FirstOrderStatsComputerSet<Iterable, DoubleType> stats = ops.op(FirstOrderStatsComputerSet.class,
+				Iterable.class);
+
+		IterableComputerSetProcessor<FloatType, DoubleType> processor = ops.op(IterableComputerSetProcessor.class,
+				Iterable.class, new ComputerSet[] { stats }, new DefaultComputerSetTableService<>());
+
+		Table<Column<DoubleType>, DoubleType> result = processor.compute1(getTestImage2D());
+
+		List<ComputerSet<?, DoubleType>> tmp = new ArrayList<>();
+		tmp.add(stats);
+		checkAllResultTableForOneComputerSet(result, tmp);
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test
+	public void IterableComputerSetProcessorSomeComputersAreActiveTest() {
+		FirstOrderStatsComputerSet<Iterable, DoubleType> stats = ops.op(FirstOrderStatsComputerSet.class,
+				Iterable.class, Arrays.asList(new Class[] { Mean.class, Max.class }));
+
+		IterableComputerSetProcessor<FloatType, DoubleType> processor = ops.op(IterableComputerSetProcessor.class,
+				Iterable.class, new ComputerSet[] { stats }, new DefaultComputerSetTableService<>());
+
+		Table<Column<DoubleType>, DoubleType> result = processor.compute1(getTestImage2D());
+
+		List<ComputerSet<?, DoubleType>> tmp = new ArrayList<>();
+		tmp.add(stats);
+		checkAllResultTableForOneComputerSet(result, tmp);
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Test
+	public void IterableComputerSetProcessorWithTwoComputerSetsTest() {
+		FirstOrderStatsComputerSet<Iterable, DoubleType> stats = ops.op(FirstOrderStatsComputerSet.class,
+				Iterable.class);
+		FirstOrderStatsComputerSet<Iterable, DoubleType> stats1 = ops.op(FirstOrderStatsComputerSet.class,
+				Iterable.class, Arrays.asList(new Class[] { Mean.class, Max.class }));
+
+		IterableComputerSetProcessor<FloatType, DoubleType> processor = ops.op(IterableComputerSetProcessor.class,
+				Iterable.class, new ComputerSet[] { stats, stats1 }, new DefaultComputerSetTableService<>());
+
+		Table<Column<DoubleType>, DoubleType> result = processor.compute1(getTestImage2D());
+
+		List<ComputerSet<?, DoubleType>> tmp = new ArrayList<>();
+		tmp.add(stats);
+		tmp.add(stats1);
+		checkResultTableForManyComputerSets(result, tmp);
+	}
+
+}
